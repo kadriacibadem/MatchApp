0cc60e20e5484b7528b20c63cf6e83b3f278c00d
traaz <angiyaz0@gmail.com>
2021-11-12 14:40:41 +0300

    .

--- app/src/main/java/com/example/matchapp/RegisterActivity.java
+++ app/src/main/java/com/example/matchapp/RegisterActivity.java
@@ -1,21 +1,107 @@
 package com.example.matchapp;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
+
+import com.example.matchapp.databinding.ActivityRegisterBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class RegisterActivity extends AppCompatActivity {
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
+    Uri imageData;
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        registerLauncher();
+
+
     }
     public void next(View View){
-        Intent intent= new Intent(RegisterActivity.this,SelectPhoto.class);
+        Intent intent= new Intent(RegisterActivity.this,SelectHobbies.class);
         startActivity(intent);
+
     }
+    public void selectImage(View view){
+
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) !=PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                    }
+                }).show();
+            }else{
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+
+            }
+        }else{
+            Intent intentToGalery=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            activityResultLauncher.launch(intentToGalery);
+        }
+
+    }
+
+    private void registerLauncher(){
+        activityResultLauncher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode()==Activity.RESULT_OK){
+                    Intent intentFromResult=result.getData();
+                    if(intentFromResult != null){
+                        imageData=intentFromResult.getData();
+                        binding.imageView4.setImageURI(imageData);
+                    }
+                }
+            }
+        });
+        permissionLauncher=registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if(result){
+                    Intent intentToGalery=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intentToGalery);
+
+                }else{
+                    Toast.makeText(RegisterActivity.this,"Permission Needed !",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
 
 }
