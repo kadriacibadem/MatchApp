d783018c90623bc0dc3db1aa2c8d810f7edfd8ec
traaz <angiyaz0@gmail.com>
2021-11-13 19:18:39 +0300

    .

---
+++ app/src/main/java/com/example/matchapp/AllUserMember.java
@@ -0,0 +1,53 @@
+package com.example.matchapp;
+
+public class AllUserMember {
+    String emaiL;
+    String name;
+    String age;
+    String url;
+    String uid;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public AllUserMember() {
+
+    }
+
+    public String getEmaiL() {
+        return emaiL;
+    }
+
+    public void setEmaiL(String emaiL) {
+        this.emaiL = emaiL;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
--- app/src/main/java/com/example/matchapp/RegisterActivity.java
+++ app/src/main/java/com/example/matchapp/RegisterActivity.java
@@ -5,24 +5,34 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.matchapp.databinding.ActivityRegisterBinding;
+
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
@@ -30,84 +40,154 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.core.Tag;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
-    ActivityResultLauncher<Intent> activityResultLauncher;
-    ActivityResultLauncher<String> permissionLauncher;
-    Uri imageData;
-    private FirebaseAuth auth;
-    private FirebaseStorage firebaseStorage;
+
+    private Uri imageUri;
+    private static final int PICK_IMAGE=1;
+    UploadTask uploadTask;
+    FirebaseStorage firebaseStorage;
+    FirebaseFirestore db=FirebaseFirestore.getInstance();
+    FirebaseDatabase database=FirebaseDatabase.getInstance();
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
+    DocumentReference documentReference;
+    TextView textView;
     ImageView profilePhoto;
+    EditText emailText, nameText, passwordText, ageText;
+    Button nextButton;
+    String userId;
+    AllUserMember member;
+    String currentUserId;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        registerLauncher();
-        profilePhoto=findViewById(R.id.imageView4);
-        firebaseStorage=FirebaseStorage.getInstance();
 
 
+        member=new AllUserMember();
+        textView = findViewById(R.id.textView3);
+        profilePhoto = findViewById(R.id.imageView4);
+        emailText = findViewById(R.id.emailText);
+        nameText = findViewById(R.id.nameText);
+        passwordText = findViewById(R.id.passwordText);
+        ageText = findViewById(R.id.ageText);
+        nextButton = findViewById(R.id.nextButton);
+
+        FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+        currentUserId=user.getUid();
+
+        documentReference=db.collection("user").document(currentUserId);
+        storageReference = FirebaseStorage.getInstance().getReference("Profile images");
+        databaseReference=database.getReference("All Users");
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadData();
+            }
+        });
+        profilePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,PICK_IMAGE);
+            }
+        });
 
     }
-        public void selectImage(View view){
 
-        if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) !=PackageManager.PERMISSION_GRANTED){
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
-                Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
-
-                    }
-                }).show();
-            }else{
-                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
-
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if(requestCode == PICK_IMAGE || resultCode == RESULT_OK || data != null || data.getData() != null){
+                imageUri=data.getData();
+                Picasso.get().load(imageUri).into(profilePhoto);
             }
-        }else{
-            Intent intentToGalery=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            activityResultLauncher.launch(intentToGalery);
+
+        }catch (Exception e){
+            Toast.makeText(this,"Error"+e,Toast.LENGTH_SHORT).show();
         }
-
     }
+    private String getFileExt(Uri uri){
+        ContentResolver contentResolver=getContentResolver();
+        MimeTypeMap mimeTypeMap=MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+    private void uploadData(){
+        String email = emailText.getText().toString();
+        String password=passwordText.getText().toString();
+        String name=nameText.getText().toString();
+        String age=ageText.getText().toString();
+        if(!TextUtils.isEmpty(email) || !TextUtils.isEmpty(name) || !TextUtils.isEmpty(age) || !TextUtils.isEmpty(password) || imageUri != null){
+           final StorageReference reference=storageReference.child(System.currentTimeMillis()+"."+ getFileExt(imageUri));
+            uploadTask=reference.putFile(imageUri);
+            Task<Uri> uriTask=uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if(!task.isSuccessful()){
+                        throw  task.getException();
 
-    private void registerLauncher(){
-        activityResultLauncher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
-            @Override
-            public void onActivityResult(ActivityResult result) {
-                if(result.getResultCode()==Activity.RESULT_OK){
-                    Intent intentFromResult=result.getData();
-                    if(intentFromResult != null){
-                        imageData=intentFromResult.getData();
-                        profilePhoto.setImageURI(imageData);
+                    }
+                    return reference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if(task.isSuccessful()){
+                        Uri dowloadUri=task.getResult();
+                        Map<String,String> profile=new HashMap<>();
+                        profile.put("email",email);
+                        profile.put("name",name);
+                        profile.put("age",age);
+                        profile.put("url",dowloadUri.toString());
+                        member.setEmaiL(email);
+                        member.setName(name);
+                        member.setAge(age);
+                        member.setUid(currentUserId);
+                        member.setUrl(dowloadUri.toString());
+                        databaseReference.child(currentUserId).setValue(member);
+
+                        documentReference.set(profile).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(RegisterActivity.this,"Now Select Hobbies",Toast.LENGTH_SHORT).show();
+                                Intent intent2=new Intent(RegisterActivity.this,SelectHobbies.class);
+                                startActivity(intent2);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(RegisterActivity.this,"failed",Toast.LENGTH_SHORT).show();
+                            }
+                        });
                     }
                 }
-            }
-        });
-        permissionLauncher=registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
-            @Override
-            public void onActivityResult(Boolean result) {
-                if(result){
-                    Intent intentToGalery=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    activityResultLauncher.launch(intentToGalery);
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
 
-                }else{
-                    Toast.makeText(RegisterActivity.this,"Permission Needed !",Toast.LENGTH_LONG).show();
                 }
-            }
-        });
+            });
+
+        }else{
+            Toast.makeText(this,"All fields required",Toast.LENGTH_SHORT).show();
+        }
     }
-
-    public void next(View View) {
-        Intent intent = new Intent(RegisterActivity.this, SelectHobbies.class);
-        startActivity(intent);
-
-    }
-
 }
